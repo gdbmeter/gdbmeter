a390c7f5049899360f9d9eab7a410dd860547412
Inverse Integral <inverseintegral42@gmail.com>
2022-03-31 20:17:23 +0200

    Add sanity test cases

---
+++ src/test/java/ch/ethz/ast/gdblancer/neo4j/Neo4JCreateGeneratorTests.java
@@ -0,0 +1,18 @@
+package ch.ethz.ast.gdblancer.neo4j;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class Neo4JCreateGeneratorTests {
+
+    @Test
+    void testCreateEntities() {
+        String query = Neo4JCreateGenerator.createEntities();
+        
+        assertNotNull(query);
+        assertFalse(query.isEmpty());
+        assertTrue(query.startsWith("CREATE"));
+    }
+
+}
