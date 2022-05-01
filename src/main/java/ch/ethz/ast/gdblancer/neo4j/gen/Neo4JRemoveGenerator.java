9c765b3fa0743201d19f7d344eb2d4ed5dd99ffd
Inverse Integral <inverseintegral42@gmail.com>
2022-05-01 21:19:09 +0200

    Generate WHERE clauses in REMOVE queries

--- src/main/java/ch/ethz/ast/gdblancer/neo4j/gen/Neo4JRemoveGenerator.java
+++ src/main/java/ch/ethz/ast/gdblancer/neo4j/gen/Neo4JRemoveGenerator.java
@@ -2,10 +2,15 @@ package ch.ethz.ast.gdblancer.neo4j.gen;
 
 import ch.ethz.ast.gdblancer.common.ExpectedErrors;
 import ch.ethz.ast.gdblancer.neo4j.Neo4JQuery;
+import ch.ethz.ast.gdblancer.neo4j.gen.ast.Neo4JExpressionGenerator;
+import ch.ethz.ast.gdblancer.neo4j.gen.ast.Neo4JVisitor;
 import ch.ethz.ast.gdblancer.neo4j.gen.schema.Neo4JDBEntity;
 import ch.ethz.ast.gdblancer.neo4j.gen.schema.Neo4JDBSchema;
+import ch.ethz.ast.gdblancer.neo4j.gen.schema.Neo4JType;
 import ch.ethz.ast.gdblancer.neo4j.gen.util.Neo4JDBUtil;
 import ch.ethz.ast.gdblancer.util.Randomization;
+
+import java.util.Map;
 
 public class Neo4JRemoveGenerator {
 
@@ -21,7 +26,6 @@ public class Neo4JRemoveGenerator {
         return new Neo4JRemoveGenerator(schema).generateRemove();
     }
 
-    // TODO: Support REMOVE based on conditions
     // TODO: Support REMOVE on nodes with different labels
     // TODO: Support REMOVE of multiple properties
     // TODO: Add RETURN clause
@@ -33,12 +37,18 @@ public class Neo4JRemoveGenerator {
         String label = schema.getRandomLabel();
         Neo4JDBEntity entity = schema.getEntityByLabel(label);
 
-        query.append(String.format("MATCH (n:%s ", label));
-        query.append(Neo4JPropertyGenerator.generatePropertyQuery(entity));
-        query.append(") ");
+        if (Randomization.smallBiasProbability()) {
+            query.append(String.format("MATCH (n:%s ", label));
+            query.append(Neo4JPropertyGenerator.generatePropertyQuery(entity));
+            query.append(")");
+        } else {
+            query.append(String.format("MATCH (n:%s)", label));
+            query.append(" WHERE ");
+            query.append(Neo4JVisitor.asString(Neo4JExpressionGenerator.generateExpression(Map.of("n", entity), Neo4JType.BOOLEAN)));
+        }
 
         String property = Randomization.fromSet(entity.getAvailableProperties().keySet());
-        query.append(String.format("REMOVE n.%s ", property));
+        query.append(String.format(" REMOVE n.%s ", property));
 
         return new Neo4JQuery(query.toString(), errors);
     }
