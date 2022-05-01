0872e44ab4b7a095c948ec3ebff0d6aac058d406
Inverse Integral <inverseintegral42@gmail.com>
2022-05-01 20:22:42 +0200

    Generate WHERE clauses in DELETE queries

--- src/main/java/ch/ethz/ast/gdblancer/neo4j/gen/Neo4JDeleteGenerator.java
+++ src/main/java/ch/ethz/ast/gdblancer/neo4j/gen/Neo4JDeleteGenerator.java
@@ -2,10 +2,15 @@ package ch.ethz.ast.gdblancer.neo4j.gen;
 
 import ch.ethz.ast.gdblancer.common.ExpectedErrors;
 import ch.ethz.ast.gdblancer.neo4j.Neo4JQuery;
+import ch.ethz.ast.gdblancer.neo4j.gen.ast.Neo4JExpressionGenerator;
+import ch.ethz.ast.gdblancer.neo4j.gen.ast.Neo4JVisitor;
 import ch.ethz.ast.gdblancer.neo4j.gen.schema.Neo4JDBEntity;
 import ch.ethz.ast.gdblancer.neo4j.gen.schema.Neo4JDBSchema;
+import ch.ethz.ast.gdblancer.neo4j.gen.schema.Neo4JType;
 import ch.ethz.ast.gdblancer.neo4j.gen.util.Neo4JDBUtil;
 import ch.ethz.ast.gdblancer.util.Randomization;
+
+import java.util.Map;
 
 public class Neo4JDeleteGenerator {
 
@@ -21,7 +26,6 @@ public class Neo4JDeleteGenerator {
         return new Neo4JDeleteGenerator(schema).generateDelete();
     }
 
-    // TODO: Support DELETE based on conditions
     // TODO: Support DELETE of relationships
     // TODO: Support DELETE of nodes of different labels
     // TODO: Add RETURN clause
@@ -33,17 +37,23 @@ public class Neo4JDeleteGenerator {
         String label = schema.getRandomLabel();
         Neo4JDBEntity entity = schema.getEntityByLabel(label);
 
-        query.append(String.format("MATCH (n:%s ", label));
-        query.append(Neo4JPropertyGenerator.generatePropertyQuery(entity));
-        query.append(") ");
+        if (Randomization.smallBiasProbability()) {
+            query.append(String.format("MATCH (n:%s ", label));
+            query.append(Neo4JPropertyGenerator.generatePropertyQuery(entity));
+            query.append(")");
+        } else {
+            query.append(String.format("MATCH (n:%s)", label));
+            query.append(" WHERE ");
+            query.append(Neo4JVisitor.asString(Neo4JExpressionGenerator.generateExpression(Map.of("n", entity), Neo4JType.BOOLEAN)));
+        }
 
         if (Randomization.getBoolean()) {
-            query.append("DETACH ");
+            query.append(" DETACH");
         } else {
             errors.addRegex("Cannot delete node<\\d+>, because it still has relationships. To delete this node, you must first delete its relationships.");
         }
 
-        query.append("DELETE n");
+        query.append(" DELETE n");
 
         return new Neo4JQuery(query.toString(), errors);
     }
