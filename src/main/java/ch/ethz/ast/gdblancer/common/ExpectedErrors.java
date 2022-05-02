8ffc94d3340fd2e796ab57ba0dafb518656dee96
Inverse Integral <inverseintegral42@gmail.com>
2022-05-02 15:43:43 +0200

    Revert "Resolve #12876"
    This reverts commit f91baa53efa57c1e356ef1b593513f22c01a4811.

--- src/main/java/ch/ethz/ast/gdblancer/common/ExpectedErrors.java
+++ src/main/java/ch/ethz/ast/gdblancer/common/ExpectedErrors.java
@@ -34,6 +34,13 @@ public class ExpectedErrors {
                     return true;
                 }
             }
+
+            // See: #12876
+            if (((QueryExecutionException) exception).getStatusCode().equals("Neo.DatabaseError.Statement.ExecutionFailed")) {
+                if (exception.getMessage().startsWith("Expected \nRegularSinglePlannerQuery(QueryGraph {Nodes: ['n'],")) {
+                    return true;
+                }
+            }
         }
 
         if (message == null) {
