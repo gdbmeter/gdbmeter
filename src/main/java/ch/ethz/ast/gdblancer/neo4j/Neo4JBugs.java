fb71822cc089b981eeceb774e424a6b977783494
Inverse Integral <inverseintegral42@gmail.com>
2022-05-12 17:05:21 +0200

    Mark flags as final

--- src/main/java/ch/ethz/ast/gdblancer/neo4j/Neo4JBugs.java
+++ src/main/java/ch/ethz/ast/gdblancer/neo4j/Neo4JBugs.java
@@ -9,21 +9,21 @@ public class Neo4JBugs {
     public static final boolean bug12866 = true;
 
     // https://github.com/neo4j/neo4j/issues/12869
-    public static boolean bug12869 = true;
+    public static final boolean bug12869 = true;
 
     // https://github.com/neo4j/neo4j/issues/12877
-    public static boolean bug12877 = true;
+    public static final boolean bug12877 = true;
 
     // https://github.com/neo4j/neo4j/issues/12879
-    public static boolean bug12879 = true;
+    public static final boolean bug12879 = true;
 
     // https://github.com/neo4j/neo4j/issues/12878
-    public static boolean bug12878 = true;
+    public static final boolean bug12878 = true;
 
     // https://github.com/neo4j/neo4j/issues/12880
-    public static boolean bug12880 = true;
+    public static final boolean bug12880 = true;
 
     // https://github.com/neo4j/neo4j/issues/12881
-    public static boolean bug12881 = true;
-    
+    public static final boolean bug12881 = true;
+
 }
