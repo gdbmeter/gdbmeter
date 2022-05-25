6007ea5bb984b0c7cdc290a02050fc949b974ed9
Inverse Integral <inverseintegral42@gmail.com>
2022-05-25 20:03:54 +0200

    Handle bug by not generating XORs

--- src/main/java/ch/ethz/ast/gdblancer/neo4j/gen/ast/Neo4JBinaryLogicalOperation.java
+++ src/main/java/ch/ethz/ast/gdblancer/neo4j/gen/ast/Neo4JBinaryLogicalOperation.java
@@ -1,5 +1,6 @@
 package ch.ethz.ast.gdblancer.neo4j.gen.ast;
 
+import ch.ethz.ast.gdblancer.neo4j.Neo4JBugs;
 import ch.ethz.ast.gdblancer.util.Randomization;
 
 public class Neo4JBinaryLogicalOperation
@@ -18,7 +19,11 @@ public class Neo4JBinaryLogicalOperation
         }
 
         public static BinaryLogicalOperator getRandom() {
-            return Randomization.fromOptions(values());
+            if (Neo4JBugs.bug12877) {
+                return Randomization.fromOptions(AND, OR);
+            } else {
+                return Randomization.fromOptions(values());
+            }
         }
 
     }
