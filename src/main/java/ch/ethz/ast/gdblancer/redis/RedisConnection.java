c672e8c84494577ed0971dd47533fced6294c4f7
Inverse Integral <inverseintegral42@gmail.com>
2022-06-09 09:58:22 +0200

    Replace pooled jedis with direct one

--- src/main/java/ch/ethz/ast/gdblancer/redis/RedisConnection.java
+++ src/main/java/ch/ethz/ast/gdblancer/redis/RedisConnection.java
@@ -3,47 +3,41 @@ package ch.ethz.ast.gdblancer.redis;
 import ch.ethz.ast.gdblancer.common.Connection;
 import ch.ethz.ast.gdblancer.common.Query;
 import redis.clients.jedis.Jedis;
-import redis.clients.jedis.JedisPool;
 import redis.clients.jedis.Response;
 import redis.clients.jedis.Transaction;
 import redis.clients.jedis.graph.ResultSet;
 
 import java.io.IOException;
-import java.util.*;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 public class RedisConnection implements Connection {
 
-    private JedisPool pool;
+    private Jedis jedis;
 
     @Override
     public void connect() throws IOException {
-        pool = new JedisPool("localhost", 6379);
-
-        try (Jedis resource = pool.getResource()) {
-            try (Transaction transaction = new Transaction(resource)) {
-                transaction.graphDelete("db");
-                transaction.exec();
-            }
-        }
+        jedis = new Jedis("localhost", 6379);
+        System.out.println(jedis.flushAll());
     }
 
     public List<Map<String, Object>> execute(Query<RedisConnection> query) {
         List<Map<String, Object>> resultRows = null;
 
-        try (Jedis resource = pool.getResource()) {
-            try (Transaction transaction = new Transaction(resource)) {
-                Response<ResultSet> response = transaction.graphQuery("db", query.getQuery());
-                transaction.exec();
-                response.get();
-            }
+        try (Transaction transaction = jedis.multi()) {
+            Response<ResultSet> response = transaction.graphQuery("db", query.getQuery());
+            transaction.exec();
+            System.out.println(response.get());
         }
 
         return resultRows;
     }
 
     @Override
-    public void close()  {
-        pool.close();
+    public void close() {
+        jedis.close();
     }
 
     // RedisGraph does not support names indices
