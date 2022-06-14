f0a07abcd8847b8e13de833ce060257c1f7af9ab
Inverse Integral <inverseintegral42@gmail.com>
2022-06-15 00:48:14 +0200

    Add missing expected error

--- src/main/java/ch/ethz/ast/gdblancer/neo4j/Neo4JQueryReplay.java
+++ src/main/java/ch/ethz/ast/gdblancer/neo4j/Neo4JQueryReplay.java
@@ -12,17 +12,21 @@ public class Neo4JQueryReplay extends QueryReplay {
     @Override
     protected void executeQueries(List<String> queries) {
         GlobalState<Neo4JConnection> state = new GlobalState<>();
+
         ExpectedErrors errors = new ExpectedErrors();
         Neo4JUtil.addFunctionErrors(errors);
         Neo4JUtil.addArithmeticErrors(errors);
         Neo4JUtil.addRegexErrors(errors);
+
+        errors.add("There already exists an index");
+        errors.add("An equivalent index already exists");
 
         try (Neo4JConnection connection = new Neo4JConnection()) {
             connection.connect();
             state.setConnection(connection);
 
             for (String query : queries) {
-                new Neo4JQuery(query, errors).execute(state);
+                System.out.println(new Neo4JQuery(query, errors).executeAndGet(state));
             }
 
         } catch (IOException e) {
