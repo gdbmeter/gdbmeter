47c9ea8631c2bbe428f858dcd1722fb7b7404a1b
Inverse Integral <inverseintegral42@gmail.com>
2022-06-17 15:53:22 +0200

    Handle ignore me exception in test case

--- src/test/java/ch/ethz/ast/gdblancer/neo4j/gen/Neo4JCreateGeneratorTests.java
+++ src/test/java/ch/ethz/ast/gdblancer/neo4j/gen/Neo4JCreateGeneratorTests.java
@@ -2,6 +2,7 @@ package ch.ethz.ast.gdblancer.neo4j.gen;
 
 import ch.ethz.ast.gdblancer.common.Query;
 import ch.ethz.ast.gdblancer.cypher.schema.CypherSchema;
+import ch.ethz.ast.gdblancer.util.IgnoreMeException;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
@@ -11,10 +12,12 @@ public class Neo4JCreateGeneratorTests {
 
     @Test
     void testCreateEntities() {
-        Query<?> query = Neo4JCreateGenerator.createEntities(CypherSchema.generateRandomSchema());
-        
-        assertNotNull(query);
-        assertTrue(query.getQuery().startsWith("CREATE") || query.getQuery().startsWith("MERGE"));
+        try {
+            Query<?> query = Neo4JCreateGenerator.createEntities(CypherSchema.generateRandomSchema());
+
+            assertNotNull(query);
+            assertTrue(query.getQuery().startsWith("CREATE") || query.getQuery().startsWith("MERGE"));
+        } catch (IgnoreMeException ignored) {}
     }
 
 }
