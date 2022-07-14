962ff94e9638681549b33051f83da7cad2c5560c
Inverse Integral <inverseintegral42@gmail.com>
2022-07-14 10:03:37 +0200

    Improve delete generator

--- src/main/java/ch/ethz/ast/gdblancer/janus/gen/JanusDeleteGenerator.java
+++ src/main/java/ch/ethz/ast/gdblancer/janus/gen/JanusDeleteGenerator.java
@@ -4,6 +4,7 @@ import ch.ethz.ast.gdblancer.common.schema.Entity;
 import ch.ethz.ast.gdblancer.common.schema.Schema;
 import ch.ethz.ast.gdblancer.janus.query.JanusQuery;
 import ch.ethz.ast.gdblancer.janus.query.JanusQueryAdapter;
+import ch.ethz.ast.gdblancer.janus.schema.JanusPredicate;
 import ch.ethz.ast.gdblancer.janus.schema.JanusType;
 import ch.ethz.ast.gdblancer.util.Randomization;
 
@@ -24,12 +25,14 @@ public class JanusDeleteGenerator {
         String label = schema.getRandomLabel();
         Entity<JanusType> entity = schema.getEntityByLabel(label);
         String property = Randomization.fromSet(entity.getAvailableProperties().keySet());
-        JanusType type = entity.getAvailableProperties().get(property);
 
-        query.append(String.format("g.V().hasLabel('%s').has('%s', neq(%s)).drop().iterate()",
+        JanusType type = entity.getAvailableProperties().get(property);
+        JanusPredicate predicate = JanusPredicate.compareTo(type);
+
+        query.append(String.format("g.V().hasLabel('%s').has('%s', %s).drop().iterate()",
                 label,
                 property,
-                JanusValueGenerator.generate(type)));
+                predicate.toString(JanusValueGenerator.generate(type))));
 
         return new JanusQuery(query.toString());
     }
